eefc59c29364368204221f76ead85e17063bc81e
Sourav <38483276+gitoutofbox@users.noreply.github.com>
2019-12-16 19:16:35 -0800

    manifest
    manifest

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+app.use(express.static('app'));
+
+const server = app.listen(8082, function () {
+    const host = server.address().address
+    const port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+});
+
+//chrome://appcache-internals/
+app.get('/appcache', (req, res) =>{
+    res.setHeader('content-type', 'text/cache-manifest');
+    const content = fs.readFileSync('manifest.appcache', 'utf8');
+    //console.log(content)
+    res.end(content);
+})
