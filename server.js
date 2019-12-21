971d3065847d21e0d1518f1cd4471ba28f091951
gitoutofbox <people.outofbox@gmail.com>
2019-12-20 18:14:49 -0800

    readme documentation

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ const server = app.listen(8082, function () {
     console.log("Example app listening at http://%s:%s", host, port)
 });
 
+
 //chrome://appcache-internals/
 app.get('/appcache', (req, res) =>{
-    res.setHeader('content-type', 'text/cache-manifest');
-    const content = fs.readFileSync('manifest.appcache', 'utf8');
-    //console.log(content)
-    res.end(content);
+    // res.setHeader('content-type', 'text/cache-manifest');
+    // const content = fs.readFileSync('manifest.appcache', 'utf8');
+    // //console.log(content)
+    // res.end(content);
+    res.end('');
 })
